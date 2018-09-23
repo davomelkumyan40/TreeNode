4c2ad4b473486f4f7c60abb57fa2de154f0cf8c4
David <davo.melkumyan40@gmail.com>
2018-09-24 02:25:12 +0400

    Tree NODE

---
+++ Tree/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Add(CreateDirectoryN(new DirectoryInfo(@"C:\Users\davom")));
+        }
+
+        private static void DirectList(TreeView view, string path)
+        {
+
+        }
+
+        static List<TreeNode> listNode = new List<TreeNode>();
+
+        private static TreeNode CreateDirectoryN(DirectoryInfo directory)
+        {
+            var treeNode = new TreeNode(directory.Name);
+            try
+            {
+                foreach (var dir in directory.GetDirectories())
+                    treeNode.Nodes.Add(CreateDirectoryN(dir));
+
+                foreach (var file in directory.GetFiles())
+                    treeNode.Nodes.Add(new TreeNode(file.Name));
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            return treeNode;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
